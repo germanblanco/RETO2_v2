13fdbb6f0c703644849a27a8db94d905e8c3b453
German Blanco <german.blanco@gmail.com>
2016-11-27 05:46:51 +0100

    Export TSUNG dashboard port to docker host.

--- load/start.sh
+++ load/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-RATE=${RATE:-10}
-HITS=${HITS:-10}
+RATE=${RATE:-1}
+HITS=${HITS:-1}
 docker rm reto1load
 if [ -z ${THESERVER+x} ]; then 
-  docker run --link reto1server -e "RATE=$RATE" -e "HITS=$HITS" -p 8091:8091 --name reto1load reto1load
+  docker run --link reto1server -e "RATE=$RATE" -e "HITS=$HITS" -P -p 8091:8091 --name reto1load reto1load
 else
-  docker run -e "THESERVER=$THESERVER" -e "RATE=$RATE" -e "HITS=$HITS" -p 8091:8091 --name reto1load reto1load
+  docker run -e "THESERVER=$THESERVER" -e "RATE=$RATE" -e "HITS=$HITS" -P -p 8091:8091 --name reto1load reto1load
 fi
