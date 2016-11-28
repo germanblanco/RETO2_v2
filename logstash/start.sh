5ec7d3190d57f844e3e29e5ef81e9ac29c886ef1
german.blanco <german.blanco@meteologica.com>
2016-11-28 11:26:17 +0100

    Now sending info to elasticsearch.

--- logstash/start.sh
+++ logstash/start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 docker rm  retologstash
-#docker run --link retoelasticsearch --name retologstash -v "$PWD"/../shared:/shared -d retologstash
-docker run -it --name retologstash -v "$PWD"/../shared:/shared retologstash
+docker run --link retoelasticsearch --name retologstash -v "$PWD"/../shared:/shared -d retologstash
